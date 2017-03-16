38a2b156b1b4f9a698d25f9e1e97a4181be5633c
MATT BRUTON <the.bruton@gmail.com>
2017-03-16 16:24:55 -0500

    added ordering to steps for courses, throwing 404 instead of 500 code when trying to access steps courses that are not in the database

--- courses/models.py
+++ courses/models.py
@@ -17,5 +17,8 @@ class Step(models.Model):
     order = models.IntegerField(default=0)
     course = models.ForeignKey(Course)
 
+    class Meta:
+        ordering = ['order',]
+
     def __str__(self):
         return self.title
